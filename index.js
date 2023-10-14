7b63ace4fd14ae2733619c0b69f12e6b97baf4e1
aarnif <aarni.fagerstrom@gmail.com>
2023-10-14 09:48:41 +0300

    Refaktoroi koodia ja tee tehtava 3.6

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 
 const PORT = 3001;
-const persons = data.persons;
 
 app.get("/", (req, res) => {
   res.redirect("/info");
@@ -13,7 +12,9 @@ app.get("/", (req, res) => {
 
 app.get("/info", (req, res) => {
   res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+    `<p>Phonebook has info for ${
+      data.persons.length
+    } people</p><p>${new Date()}</p>`
   );
 });
 
@@ -23,7 +24,7 @@ app.get("/api/persons", (req, res) => {
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
+  const person = data.persons.find((person) => person.id === id);
 
   if (!person) {
     console.log("Person does not exist!");
@@ -36,9 +37,9 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const newPersons = persons.filter((person) => person.id !== id);
+  data.persons = data.persons.filter((person) => person.id !== id);
   console.log(`Deleted person with id ${id}`);
-  console.log(newPersons);
+  console.log(data.persons);
   res.send(`Deleted person with id ${id}`);
 });
 
@@ -48,9 +49,32 @@ app.post("/api/persons", (req, res) => {
     number: req.body.number,
     id: Math.floor(Math.random() * 100000),
   };
+
+  if (newPerson.name.length === 0) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (newPerson.number.length === 0) {
+    return res.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  const personExists = data.persons.find(
+    (person) => person.name === newPerson.name
+  );
+
+  if (personExists) {
+    console.log();
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
+  data.persons.push(newPerson);
   console.log(newPerson);
-  console.log(persons);
-  res.send(`Added person with id ${newPerson.id}`);
+  console.log(data.persons);
+  res.json(newPerson);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
